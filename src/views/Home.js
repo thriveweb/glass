509bbcc9bdbdaffaa890b144f656d66d6f9adaea
Amber Gowans <amber@thriveweb.com.au>
2018-04-17 10:14:16 +1000

    add home

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,16 +2,13 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Banner from '../components/Banner'
-import ImageGallery from '../components/ImageGallery'
 
 export default ({ page, globalSettings }) => {
-  const { featuredImage, title, intro, galleryImages, columns, contact } = page
-  const { phone, address, email, latitude, longitude } = globalSettings
+  const { featuredImage, title, content, buttonText, buttonUrl } = page
 
   return (
     <main className='Home'>
-      <Banner image={featuredImage} heading={title} address={address} phone={phone} />
-      <ImageGallery galleryItems={galleryImages} />
+      <Banner image={featuredImage} heading={title} content={content} buttonText={buttonText} buttonUrl={buttonUrl} />
       <Helmet>
         <title>{title}</title>
       </Helmet>
