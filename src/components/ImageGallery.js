da230f0d8987f607b32d488e5423ad297964790c
Amber Gowans <amber@thriveweb.com.au>
2018-04-20 15:38:40 +1000

    masonry gallery

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './ImageGallery.css'
 import Masonry from 'react-masonry-component';
 import BackgroundImage from './BackgroundImage'
 
 
-export default ({galleryItems}) => (
-	<section className='section--image-gallery'>
-		{galleryItems.map((galleryItem, index) => {
-			return <div key={galleryItem.title} className='section--image-gallery-item section-image'>
-				{galleryItem.image ? <BackgroundImage src={galleryItem.image} imageSize='550' /> : ''}
-				<h3>{galleryItem.title}</h3>
-			</div>
-		})}
-	</section>
-)
+const masonryOptions = {
+    transitionDuration: 0
+};
 
-	<Masonry
-        className={'my-gallery-class'} // default ''
-        elementType={'ul'} // default 'div'
-        options={masonryOptions} // default {}
-        disableImagesLoaded={false} // default false
-        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-        imagesLoadedOptions={imagesLoadedOptions} // default {}
-    >
-        {galleryItems}
-    </Masonry>
+class ImageGallery extends Component {
+	render() {
+		const { galleryItems } = this.props
+
+		return (
+			<section className='section--image-gallery'>
+				<div className='container'>
+					<div className='section--image-gallery-heading'>
+						<p className='title'>Our Models</p>
+						<h2>Explore Our Models</h2>
+					</div>
+					<Masonry
+						className={'section--gallery-items'} // default ''
+						elementType={'div'} // default 'div'
+						options={masonryOptions} // default {}
+						disableImagesLoaded={false} // default false
+						updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+					>
+						{galleryItems.map((galleryItem, index) => {
+							return <a href='/' key={galleryItem.title} className='section--image-gallery-item'>
+								{galleryItem.image ? <BackgroundImage src={galleryItem.image} imageSize='600' /> : ''}
+								<h3>{galleryItem.title}</h3>
+							</a>
+						})}
+					</Masonry>
+				</div>	
+			</section>
+		)        
+	}
+}
+
+export default ImageGallery
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,12 +5,12 @@ import Banner from '../components/Banner'
 import ImageGallery from '../components/ImageGallery'
 
 export default ({ page, globalSettings }) => {
-  const { featuredImage, title, content, buttonText, buttonUrl } = page
+  const { featuredImage, title, content, buttonText, buttonUrl, galleryImages} = page
 
   return (
     <main className='Home'>
       <Banner image={featuredImage} heading={title} content={content} buttonText={buttonText} buttonUrl={buttonUrl} />
-      <ImageGallery />
+      <ImageGallery galleryItems={galleryImages} />
       <Helmet>
         <title>{title}</title>
       </Helmet>
