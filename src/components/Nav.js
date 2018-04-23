8ee572d7a32778ff123a12c6ba8ce33713c140c4
Amber Gowans <amber@thriveweb.com.au>
2018-04-24 08:29:06 +1000

    slider

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import Logo from './Logo'
@@ -7,15 +7,22 @@ import './Nav.css'
 import {ICONLogin} from './Icons'
 
 
-export default ({ handlePopupOpen, header }) => (
+class Nav extends Component {
+	render() {
+		const { handlePopupOpen, header } = this.props
 
-	<nav className='nav'>
-	 	<div className='nav--container container'>
-	      	<a href='/'><Logo /></a>
-	      	<div className='client-nav'>
-	      		<a className='title' href='/'><ICONLogin /> Join Us</a>
-	      		<a className='button' href={`${header.buttonUrl}`}>{header.buttonText}</a>
-	      	</div>	
-	  	</div>
-	</nav> 
-)
+		return (
+			<nav className='nav'>
+			 	<div className='nav--container container'>
+			      	<a href='/'><Logo /></a>
+			      	<div className='client-nav'>
+			      		<a className='title' href='/'><ICONLogin /> Join Us</a>
+			      		<a className='button' href={`${header.buttonUrl}`}>{header.buttonText}</a>
+			      	</div>	
+			  	</div>
+			</nav> 
+		)
+	}
+}
+
+export default Nav
--- src/components/SectionTestimonials.js
+++ src/components/SectionTestimonials.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import LazyImage from './LazyImage'
 import './SectionTestimonials.css'
 import {ICONQuotes} from './Icons'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
 
-export default ({ title, testimonials }) =>
-	<section className='section--testimonials'>
-		<div className='container'>
-			<ICONQuotes />
-			{ title && <p className='title'>{title}</p> }
-			<div className='section--testimonials-slider'>
-				{ testimonials.map(testimonial => {
-					return <div className='section--testimonials-item testimonial-slide'>
-						{ testimonial.title && <h2>{testimonial.title}</h2> }
-						{ testimonial.content && <p className='section--testimonials-item-content'>{testimonial.content}</p> }
-						{ testimonial.logo && <LazyImage src={`${testimonial.logo}`} imageSize='300' /> }
-						{ testimonial.name && <p className='title'>{testimonial.name}</p> }
-					</div>
-				})}
-			</div>	
-		</div>
-	</section>
+
+class SimpleSlider extends Component {
+	render() {
+
+		const {title, testimonials} = this.props
+
+		var settings = {
+	      dots: false,
+	      infinite: true,
+	      speed: 500,
+	      slidesToShow: 1,
+	      slidesToScroll: 1
+	    };
+
+		return (
+			<Slider className='section--testimonials' {...settings}>
+				<div className='container'>
+					<ICONQuotes />
+					{ title && <p className='title'>{title}</p> }
+					<div className='section--testimonials-slider'>
+						{ testimonials.map(testimonial => {
+							return <div className='section--testimonials-item testimonial-slide'>
+								{ testimonial.title && <h2>{testimonial.title}</h2> }
+								{ testimonial.content && <p className='section--testimonials-item-content'>{testimonial.content}</p> }
+								{ testimonial.logo && <LazyImage src={`${testimonial.logo}`} imageSize='300' /> }
+								{ testimonial.name && <p className='title'>{testimonial.name}</p> }
+							</div>
+						})}
+					</div>	
+				</div>
+			</Slider>
+		)
+	}
+}
+
+export default SimpleSlider
