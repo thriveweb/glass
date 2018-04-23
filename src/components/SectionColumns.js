99c72b43439a0bf14261a59ea64e75a9d2f22800
Amber Gowans <amber@thriveweb.com.au>
2018-04-23 11:56:58 +1000

    columns styling

--- src/components/SectionColumns.js
+++ src/components/SectionColumns.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react'
 
 import LazyImage from './LazyImage'
+import './SectionColumns.css'
 
-class SectionColumns extends Component {
-	render() {
-		const { columns } = this.props
+export default ({ columns }) =>
 
-		return (
-			<section className='section--columns'>
-				<div className='container'>
-					{columns.map(column => {
-						return <div key={`${columns.title}`} className='section--columns-item'>
-							{ column.image && <LazyImage src={`${column.image}`} /> }
-							<div className='section--columns-item-content'>
-								{ column.title && <p className='title'>{column.title}</p> }
-								{ column.content && <h2>{column.content}</h2> }
-								{ column.buttonTitle && column.buttonUrl && <a className='button button-black' href={`${column.buttonUrl}`}>{column.buttonTitle}</a> }
-							</div>
-						</div>
-					})}
-				</div>
-			</section>
-		)
-	}
-}
-
-export default SectionColumns
+	<section className='section--columns'>
+		{columns.map(column => {
+			return <div key={`${columns.title}`} className={`section--columns-item ${!column.leftAlign ? 'column-right' : ''}`}>
+				<div className='container skinny'>
+					{ column.image && <LazyImage src={`${column.image}`} /> }
+					<div className='section--columns-item-content'>
+						{ column.title && <p className='title'>{column.title}</p> }
+						{ column.content && <h2>{column.content}</h2> }
+						{ column.buttonTitle && column.buttonUrl && <a className='button button-black' href={`${column.buttonUrl}`}>{column.buttonTitle}</a> }
+					</div>
+				</div>	
+			</div>
+		})}
+	</section>
