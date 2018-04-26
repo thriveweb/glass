5188ffdcbe42f8abee459ceb45bb951210de4acb
Amber Gowans <amber@thriveweb.com.au>
2018-04-26 11:08:13 +1000

    sub titles

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,12 +3,12 @@ import React from 'react'
 import BackgroundImage from './BackgroundImage'
 import './Banner.css'
 
-export default ({image, heading, content, buttonText, buttonUrl}) => (
+export default ({image, heading, subHeading, content, buttonText, buttonUrl}) => (
 
 	<section className='section--banner relative'>
 		{image && <BackgroundImage src={image} imageSize='1800' />}
 		<div className='container relative'>
-			<p className='title'>Welcome To</p>
+			<p className='title'>{subHeading}</p>
 			{heading && <h1>{heading}</h1>}
 			{content && <p className='section--banner-content'>{content}</p>}
 			{buttonUrl && buttonText && <a className='button' href={`${buttonUrl}`}>{buttonText}</a>}
