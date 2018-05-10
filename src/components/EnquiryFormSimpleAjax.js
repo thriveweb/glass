22aca62eb36e4b9001fa7eb4a5c09d1f77c2d400
Amber Gowans <amber@thriveweb.com.au>
2018-05-11 08:19:59 +1000

    form update

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -145,7 +145,7 @@ class Form extends React.Component {
                 className='EnquiryForm--Input'
                 type='file'
                 placeholder='Upload Photo'
-                name='upload-photo'
+                name='upload-bodyshot'
                 onChange={event => this.handleUpload(event, 'bodyShot')}
                 required
               />
@@ -159,7 +159,7 @@ class Form extends React.Component {
                 className='EnquiryForm--Input'
                 type='file'
                 placeholder='Upload Photo'
-                name='upload-photo'
+                name='upload-headshot'
                 onChange={event => this.handleUpload(event, 'headShot')}
                 required
               />
