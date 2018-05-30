93bcc06477ff5607cb2de3d7e97256c890cae1ca
Eric Jinks <ericjinks@gmail.com>
2018-05-30 15:40:05 +1000

    Update Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,104 +1,126 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import _throttle from 'lodash/throttle'
 import './Nav.css'
 
 import { slugify } from '../util/url'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Logo from './Logo'
 import LogoScroll from './LogoScroll'
 
-import {ICONLogin} from './Icons'
-
+import { ICONLogin } from './Icons'
 
 class Nav extends Component {
+  state = {
+    navActive: null,
+    mobileActive: false,
+    menuItemActive: false
+  }
 
-	state = {
-		navActive: null,
-		mobileActive: false,
-		menuItemActive: false
-	}
+  onMobileClick = () => {
+    this.setState({
+      mobileActive: !this.state.mobileActive
+    })
+  }
 
-	onMobileClick = () => {
-		this.setState({
-			mobileActive: !this.state.mobileActive
-		})
-	}
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.props.location !== prevProps.location) {
+      this.setState({
+        mobileActive: false
+      })
+    }
+  }
 
-	componentDidUpdate = (prevProps, prevState) => {
-		if (this.props.location !== prevProps.location) {
-	      this.setState({
-	      	mobileActive: false
-	      })
-	    }
-	}
+  componentDidMount = () => {
+    window.addEventListener('scroll', _throttle(this.calcNavPosition, 50))
+  }
 
-	onMenuItemClick = () => {
-		this.setState({
-			menuItemActive: !this.state.menuItemActive
-		})
-	}
+  onMenuItemClick = () => {
+    this.setState({
+      menuItemActive: !this.state.menuItemActive
+    })
+  }
 
-	componentDidMount = () => {
-		window.addEventListener('scroll', () => {
+  calcNavPosition = () => {
+    if (window.pageYOffset > 20 && !this.state.navActive) {
+      this.setState({
+        navActive: true
+      })
+    } else if (window.pageYOffset < 20 && this.state.navActive) {
+      this.setState({
+        navActive: null
+      })
+    }
+  }
 
-			if(window.pageYOffset > 20 && !this.state.navActive) {
-				this.setState({
-					navActive: true
-				})
-			} else if(window.pageYOffset < 20 && this.state.navActive) {
-				this.setState({
-					navActive: null
-				})
-			}
-		})
-	}
+  render () {
+    const { header, modelTypes = [] } = this.props
+    const { mobileActive, navActive, menuItemActive } = this.state
 
-	render() {
-		const { header, modelTypes=[] } = this.props
-		const { mobileActive, navActive, menuItemActive } = this.state
-
-		return (
-			<nav className={`nav ${navActive ? 'active' : ''} ${mobileActive ? 'mobile-active' : ''}`}>
-			 	<div className='nav--container container'>
-			      	<Link to='/'><Logo /></Link>
-			      	<Link to='/'><LogoScroll /></Link>
-			      	<nav>
-			      		<ul>
-			      			<li><Link to='/about'>About</Link></li>
-			      			<li 
-			      				onClick={this.onMenuItemClick}
-			      				className={`menu-item-has-children ${menuItemActive ? 'active' : ''}`}>
-			      				<span className='menu-item-content'>
-			      					Models <span className='dropdown-arrow'>&#x25BE;</span>
-			      				</span>
-			      				<ul className='subMenu'>
-					      			{modelTypes.map(selectedModelType => {
-					      				return <li key={selectedModelType.name}>
-					      					<Link to={`/models/${selectedModelType.name}`}>{selectedModelType.name}</Link>
-					      				</li>
-					      			})}
-				      			</ul>
-			      			</li>
-			      			<li><Link to='/blog'>Blog</Link></li>
-			      			<li><Link to='/contact'>Contact</Link></li>
-			      		</ul>
-			      	</nav>
-			      	<div className='client-nav'>
-			      		<Link className='title' to='/join-us'><ICONLogin /> Join Us</Link>
-			      		<Link className='button' to={slugify(`/${header.buttonUrl}`)}>{header.buttonText}</Link>
-			      	</div>	
-				   	<div 
-				   		id="mobile-menu" 
-				   		onClick={this.onMobileClick}
-				   	>
-	                    <span></span>
-	                    <span></span>
-	                    <span></span>
-	                </div>
-			  	</div>
-			</nav> 
-		)
-	}
+    return (
+      <nav
+        className={`nav ${navActive ? 'active' : ''} ${
+          mobileActive ? 'mobile-active' : ''
+        }`}
+      >
+        <div className='nav--container container'>
+          <Link to='/'>
+            <Logo />
+          </Link>
+          <Link to='/'>
+            <LogoScroll />
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li
+                onClick={this.onMenuItemClick}
+                className={`menu-item-has-children ${
+                  menuItemActive ? 'active' : ''
+                }`}
+              >
+                <span className='menu-item-content'>
+                  Models <span className='dropdown-arrow'>&#x25BE;</span>
+                </span>
+                <ul className='subMenu'>
+                  {modelTypes.map(selectedModelType => {
+                    return (
+                      <li key={selectedModelType.name}>
+                        <Link to={`/models/${selectedModelType.name}`}>
+                          {selectedModelType.name}
+                        </Link>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </li>
+              <li>
+                <Link to='/blog'>Blog</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className='client-nav'>
+            <Link className='title' to='/join-us'>
+              <ICONLogin /> Join Us
+            </Link>
+            <Link className='button' to={slugify(`/${header.buttonUrl}`)}>
+              {header.buttonText}
+            </Link>
+          </div>
+          <div id='mobile-menu' onClick={this.onMobileClick}>
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default withRouter(Nav)
