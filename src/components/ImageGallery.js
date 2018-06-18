5991c10879b7e76bf05ba120a8c48601df17f9f7
Amber Gowans <amber@thriveweb.com.au>
2018-06-18 11:15:53 +1000

    new masonry layout

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react'
 import './ImageGallery.css'
 import _kebabCase from 'lodash/kebabCase'
 import { Link } from 'react-router-dom'
-import Masonry from 'react-masonry-component'
+// import Masonry from 'react-masonry-component'
 import BackgroundImage from './BackgroundImage'
-
-// const masonryOptions = {
-//     transitionDuration: 0
-// };
 
 class ImageGallery extends Component {
   render () {
@@ -21,31 +17,23 @@ class ImageGallery extends Component {
             {subTitle && <p className='title'>{subTitle}</p>}
             {title && <h2>{title}</h2>}
           </div>
-          <Masonry
-            className={'section--gallery-items'} // default ''
-            elementType={'div'} // default 'div'
-            // options={masonryOptions} // default {}
-            // disableImagesLoaded={false} // default false
-            // updateOnEachImageLoad={true} // default false and works only if disableImagesLoaded is false
-          >
-            {galleryImages.map((galleryItem, index) => {
-              return (
-                <Link
-                  to={`/models/${_kebabCase(galleryItem.title)}`}
-                  key={galleryItem.title}
-                  className='section--image-gallery-item'
-                >
-                  {galleryItem.image && (
-                    <BackgroundImage
-                      src={galleryItem.image}
-                      imageSize={window.innerWidth}
-                    />
-                  )}
-                  <h3 className='title-fancy'>{galleryItem.title}</h3>
-                </Link>
-              )
-            })}
-          </Masonry>
+          {galleryImages && galleryImages.map((galleryItem, index) => {
+            return (
+              <Link
+                to={`/models/${_kebabCase(galleryItem.title)}`}
+                key={galleryItem.title}
+                className='section--image-gallery-item'
+              >
+                {galleryItem.image && (
+                  <BackgroundImage
+                    src={galleryItem.image}
+                    imageSize={window.innerWidth}
+                  />
+                )}
+                <h3 className='title-fancy'>{galleryItem.title}</h3>
+              </Link>
+            )
+          })}
         </div>
       </section>
     )
