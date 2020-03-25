b96962ea8a9bd8bd4ebf56ce7bac4f8cfe043b67
Amber Gowans <amber@thriveweb.com.au>
2020-03-25 10:48:26 -0700

    botfield

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -98,8 +98,8 @@ handleSubmit = e => {
         name={name}
         method='post'
         onSubmit={this.handleSubmit}
-        netlify-honeypot='bot-field'
         data-netlify='true'
+        data-netlify-honeypot='_gotcha'
         style={hidden ? { display: 'none' } : {}}
       >
         <h2 className='form-description'>Please Submit your details here</h2>
@@ -210,7 +210,7 @@ handleSubmit = e => {
         )}
 
         <div className='form--footer'>
-          <input name='bot-field' />
+          <input type='text' name='_gotcha' style={{ display: 'none' }} />
           {!!subject && <input type='hidden' name='subject' value={subject} />}
           <input type='hidden' name='form-name' value={name} />
           <input
