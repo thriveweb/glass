c8d04589e9266e1fa438a30b1bbb1f3d98b32394
deanoakley <dean@thriveweb.com.au>
2021-02-26 10:10:58 +1000

    join form update

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -11,7 +11,8 @@ class Form extends React.Component {
     subject: '', // optional subject of the notification email
     action: '',
     hidden: false,
-    successMessage: 'Thank you for your submission, we will be in contact with you shortly',
+    successMessage:
+      'Thank you for your submission, we will be in contact with you shortly',
     errorMessage:
       'There is a problem, your message has not been sent, please try contacting us via email'
   }
@@ -21,181 +22,186 @@ class Form extends React.Component {
     disabled: false
   }
 
-handleUpload = e => {
-  const file = e.target.files[0]
-    ? e.target.files[0]
-    : this.state[e.target.name]
-
-  this.setState({
-    [e.target.name]: file
-  })
-}
-
-handleChange = e => {
-  this.setState({
-    [e.target.name]: e.target.value
-  })
-}
-
-handleEncode = data => {
-  const formData = new FormData();
-
-  for (const key of Object.keys(data)) {
-    formData.append(key, data[key]);
-  }
-
-  return formData;
-}
-
-handleSubmit = e => {
-  e.preventDefault()
-  if (this.state.disabled) return
-  const form = e.target
-  this.setState({ disabled: true })
-
-  const { alert, disabled, ...data } = this.state
-
-  if (!data['headshot'] || !data['bodyshot']) {
-    return this.setState({
-      alert: 'Please attach both headshot & bodyshot'
-    })
-  } else {
-    console.log(data)
+  handleUpload = e => {
+    const file = e.target.files[0]
+      ? e.target.files[0]
+      : this.state[e.target.name]
 
     this.setState({
-      filesUploading: true
-    }, () => {
-      fetch("/", {
-        method: "POST",
-        body: this.handleEncode({ "form-name": "Join Us Form", ...data })
-      })
-      .then(() => {
-        form.reset()
-        this.setState({
-          alert: this.props.successMessage,
-          filesUploading: false,
-          disabled: false
-        })
-      })
-      .catch(err => {
-        this.setState({
-          disabled: false,
-          alert: this.props.errorMessage,
-          filesUploading: false
-        })
-      })
+      [e.target.name]: file
     })
   }
-}
 
-  render () {
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleEncode = data => {
+    const formData = new FormData()
+
+    for (const key of Object.keys(data)) {
+      formData.append(key, data[key])
+    }
+
+    return formData
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.disabled) return
+    const form = e.target
+    this.setState({ disabled: true })
+
+    const { alert, disabled, ...data } = this.state
+
+    // if (!data['headshot'] || !data['bodyshot']) {
+    //   return this.setState({
+    //     alert: 'Please attach both headshot & bodyshot'
+    //   })
+    // } else {
+    //   console.log(data)
+
+    this.setState(
+      {
+        filesUploading: true
+      },
+      () => {
+        fetch('/', {
+          method: 'POST',
+          body: this.handleEncode({ 'form-name': 'Join Us Form', ...data })
+        })
+          .then(() => {
+            form.reset()
+            this.setState({
+              alert: this.props.successMessage,
+              filesUploading: false,
+              disabled: false
+            })
+          })
+          .catch(err => {
+            this.setState({
+              disabled: false,
+              alert: this.props.errorMessage,
+              filesUploading: false
+            })
+          })
+      }
+    )
+    // }
+  }
+
+  render() {
     const { name, subject, hidden } = this.props
     const { filesUploading } = this.state
 
     return (
       <form
-        className='EnquiryForm'
+        className="EnquiryForm"
         name={name}
-        method='post'
+        method="post"
         onSubmit={this.handleSubmit}
-        data-netlify='true'
-        data-netlify-honeypot='_gotcha'
+        data-netlify="true"
+        data-netlify-honeypot="_gotcha"
         style={hidden ? { display: 'none' } : {}}
       >
-        <h2 className='form-description'>Please Submit your details here</h2>
-        <label className='EnquiryForm--Label'>
+        <h2 className="form-description">Please Submit your details here</h2>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='text'
-            placeholder='Full Name'
-            name='name'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Full Name"
+            name="name"
             onChange={this.handleChange}
             required
           />
         </label>
-        <label className='EnquiryForm--Label'>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='text'
-            placeholder='Email'
-            name='email'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Email"
+            name="email"
             onChange={this.handleChange}
             required
           />
         </label>
-        <label className='EnquiryForm--Label'>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='text'
-            placeholder='Phone'
-            name='phone'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Phone"
+            name="phone"
             onChange={this.handleChange}
             required
           />
         </label>
-        <label className='EnquiryForm--Label'>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='text'
-            placeholder='Age'
-            name='age'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Age"
+            name="age"
             onChange={this.handleChange}
             required
           />
         </label>
-        <label className='EnquiryForm--Label'>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='text'
-            placeholder='Height'
-            name='height'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Height"
+            name="height"
             onChange={this.handleChange}
             required
           />
         </label>
-        <label className='EnquiryForm--Label'>
+        <label className="EnquiryForm--Label">
           <input
-            className='EnquiryForm--Input'
-            type='url'
-            placeholder='Instagram'
-            name='instagram'
+            className="EnquiryForm--Input"
+            type="text"
+            placeholder="Instagram"
+            name="instagram"
             onChange={this.handleChange}
           />
         </label>
-        <label className='EnquiryForm--Label textarea'>
+        <label className="EnquiryForm--Label textarea">
           <textarea
-            className='EnquiryForm--Input EnquiryForm--Textarea'
-            placeholder='Experience'
-            name='experience'
-            rows='10'
+            className="EnquiryForm--Input EnquiryForm--Textarea"
+            placeholder="Experience"
+            name="experience"
+            rows="10"
             onChange={this.handleChange}
             required
           />
         </label>
-        <div className='file-download'>
-          <div className='file-download-item'>
-            <label className='EnquiryForm--Label title'>
+        <div className="file-download">
+          <div className="file-download-item">
+            <label className="EnquiryForm--Label title">
               <input
-                className='EnquiryForm--Input'
-                type='file'
-                accept='image/*'
-                placeholder='Upload Photo'
-                name='bodyshot'
+                className="EnquiryForm--Input"
+                type="file"
+                accept="image/*"
+                placeholder="Upload Photo"
+                name="bodyshot"
                 onChange={this.handleUpload}
+                required
               />
               <span>Upload Photo</span> please attach a full length bodyshot
             </label>
             {this.state.bodyshot && <p>{this.state.bodyshot.name}</p>}
           </div>
-          <div className='file-download-item'>
-            <label className='EnquiryForm--Label title'>
+          <div className="file-download-item">
+            <label className="EnquiryForm--Label title">
               <input
-                className='EnquiryForm--Input'
-                type='file'
-                accept='image/*'
-                placeholder='Upload Photo'
-                name='headshot'
+                className="EnquiryForm--Input"
+                type="file"
+                accept="image/*"
+                placeholder="Upload Photo"
+                name="headshot"
                 onChange={this.handleUpload}
+                required
               />
               <span>Upload Photo</span> please attach a current headshot
             </label>
@@ -204,20 +210,23 @@ handleSubmit = e => {
         </div>
 
         {this.state.alert && (
-          <div className='EnquiryForm--Alert'>
-            {this.state.alert}
-          </div>
+          <div className="EnquiryForm--Alert">{this.state.alert}</div>
         )}
 
-        <div className='form--footer'>
-          {!!subject && <input type='hidden' name='subject' value={subject} />}
-          <input type='hidden' name='form-name' value={name} />
+        <div className="form--footer">
+          {!!subject && <input type="hidden" name="subject" value={subject} />}
+          <input type="hidden" name="form-name" value={name} />
           <input
-            className='button EnquiryForm--SubmitButton'
-            type='submit'
+            className="button EnquiryForm--SubmitButton"
+            type="submit"
             value={!filesUploading ? 'Send' : 'Uploading Files...'}
           />
-          <input type='text' name='_gotcha' onChange={this.handleChange} style={{ opacity: 0, pointerEvents: 'none'}} />
+          <input
+            type="text"
+            name="_gotcha"
+            onChange={this.handleChange}
+            style={{ opacity: 0, pointerEvents: 'none' }}
+          />
         </div>
       </form>
     )
