9a8d52eabd36cade9cfe4de961e1519e4ff95475
deanoakley <dean@thriveweb.com.au>
2021-02-26 15:37:49 +1000

    lazy image class remove

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -3,31 +3,34 @@ import PropTypes from 'prop-types'
 import './LazyImage.css'
 
 class LazyImage extends React.Component {
-
-  render () {
-    const { sizes, onClick, alt, src} = this.props
+  render() {
+    const { sizes, onClick, alt, src } = this.props
     let className = this.props.className || ''
 
     let imgSrc = src
 
-    if(sizes) {
-      if(src.includes('imgix')) {
+    if (sizes) {
+      if (src.includes('imgix')) {
         imgSrc = imgSrc.includes('webp')
-          ? `${imgSrc.replace('https://glassmanagement.imgix.net/images/uploads/', 'https://glassmanagement.imgix.net/images/uploads/resized/')}.png?auto=compress,format&w=${sizes}&fm=png`
+          ? `${imgSrc.replace(
+              'https://glassmanagement.imgix.net/images/uploads/',
+              'https://glassmanagement.imgix.net/images/uploads/resized/'
+            )}.png?auto=compress,format&w=${sizes}&fm=png`
           : `${imgSrc}?auto=compress,format&w=${sizes}&fm=png`
       } else {
         imgSrc = `${imgSrc}-/resize/${sizes}x/`
       }
     }
 
-    return <img
-      className={`LazyImage ${className}`}
-      src={imgSrc}
-      sizes={sizes || '100vw'}
-      onClick={onClick}
-      alt={alt}
-    />
-
+    return (
+      <img
+        className={`XLazyImage ${className}`}
+        src={imgSrc}
+        sizes={sizes || '100vw'}
+        onClick={onClick}
+        alt={alt}
+      />
+    )
   }
 }
 
